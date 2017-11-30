5f969a71ff59adc79a49ffec4747602103ff56a6
vinothnageswaran <vinothnageswaran@192-168-1-11.tpgi.com.au>
2017-11-30 19:30:01 +0800

    changes1 to slave12

--- Factory Pattern/src/Circle.java
+++ Factory Pattern/src/Circle.java
@@ -1,5 +1,5 @@
 public class Circle implements Shape {
-//changes1 to slave1
+//changes1 to slave12
    @Override
    public void draw() {
       System.out.println("Inside Circle::draw() method.");
