7965dcf891e0c2b26c4fda31055bdf0e9aa92ef0
vinothnageswaran <vinothnageswaran@192-168-1-11.tpgi.com.au>
2017-11-30 19:39:50 +0800

    changes in newrelease

--- Factory Pattern/src/Circle.java
+++ Factory Pattern/src/Circle.java
@@ -1,5 +1,5 @@
 public class Circle implements Shape {
-//changes1 to slave12
+//changes in newrelease
    @Override
    public void draw() {
       System.out.println("Inside Circle::draw() method.");
